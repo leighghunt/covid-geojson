3b140d6e640a38a11e7c34a32ee59621986acdad
Glitch (glitch-hello-sqlite) <none>
2021-08-20 02:55:53 +0000

    🚲🐢 Checkpoint ./server.js:72439/110

--- server.js
+++ server.js
@@ -196,6 +196,13 @@ fastify.listen(process.env.PORT, function(err, address) {
 var locationsOfInterestURL = "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-health-advice-public/contact-tracing-covid-19/covid-19-contact-tracing-locations-interest"
 // var LOIs = []
 
+
+cron.schedule('*/1 * * * *', () => {
+  getLocationsOfInterest();
+});
+
+
+
 function getLocationsOfInterest(){
     axios.get(locationsOfInterestURL, {
     }
